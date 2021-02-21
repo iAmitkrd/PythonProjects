08465e987b1070f6fffc2c177dcbcb468af0a099
Amit Dubey <amitd079@gmail.com>
2021-02-21 20:06:02 +0530

    Whatsapp Automation using pywhatkit for Exploring various functions

---
+++ WhatsappAutomation/whatsapp.py
@@ -0,0 +1,5 @@
+import pywhatkit
+
+# pywhatkit.sendwhatmsg("+918291409288", "Tu apna bhai hai", 23, 24, 5)
+
+pywhatkit.sendwhatmsg_to_group("HSNPU2bh46jL2PVOBGrNJl", "tum log superstar hai", 1, 3, 5)
